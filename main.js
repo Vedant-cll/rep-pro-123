6b76027d89ce6a37a96624a699c438a5fca2b8c4
VV <85766798+Vedant-cll@users.noreply.github.com>
2022-02-11 17:36:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    video = createCapture(VIDEO);
+video.size = (500, 500);
+canvas = createCanvas(500, 500);
+canvas.position(700,60);
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialized');
+    
+    }
+    
+    function gotPoses(results){
+        if(results.length > 0){
+    console.log(results);
+        }
+    }
